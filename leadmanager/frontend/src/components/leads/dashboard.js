d43b9bab6727dcb78d4f87c45cfcde6326beeecf
Kumail <kumail0691@gmail.com>
2021-09-17 16:31:21 +0500

    Added React Components

---
+++ leadmanager/frontend/src/components/leads/dashboard.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import Form from './form';
+import Leads from './Leads';
+
+
+
+export default function dashboard() {
+    return (
+        <Fragment>
+            <Form />
+            <Leads />
+        </Fragment>
+    )
+}
---
+++ leadmanager/frontend/src/components/leads/form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class form extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Add Lead form</h1>
+            </div>
+        )
+    }
+}
+
+export default form
